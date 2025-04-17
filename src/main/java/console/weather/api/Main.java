bbf4f157dd7f8bb5c46cf62a96c15d165ec3a9d8
skrillex2007 <dropdopefam@gmail.com>
2025-04-17 21:58:13 +0300

    Is changed Current.java and fixed the bugs, will make another options and i'll add JFrame for make an interface

--- src/main/java/console/weather/api/Main.java
+++ src/main/java/console/weather/api/Main.java
@@ -6,7 +6,7 @@ import retrofit2.Response;
 import java.io.IOException;
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("Weather Api you dit Hello!");
         Scanner scanner = new Scanner(System.in);
 
@@ -16,7 +16,12 @@ public class Main {
             System.out.print("Enter your city (or click ctrl+c for exit): ");
             String str = scanner.nextLine();
 
-            Response<WeatherResponse> response = Weather.instance().current(Weather.KEY, str).execute();
+            Response<WeatherResponse> response = null;
+            try {
+                response = Weather.instance().current(Weather.KEY, str).execute();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             if(response.isSuccessful()){
                 Current current = response.body().getCurrent();
                 System.out.println("Temperature is " + current.getTempC() + " and it feels like " + current.getFeelslikeC());
--- src/main/java/console/weather/api/dto/Current.java
+++ src/main/java/console/weather/api/dto/Current.java
@@ -1,53 +1,265 @@
 package console.weather.api.dto;
 
 import com.google.gson.annotations.SerializedName;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class Current {
-    private Double lastUpdateEpoch;
-    @SerializedName("last_update_epoch")
-    private String lastUpdate;
-    @SerializedName("last_update")
-    private Double tempC;
+    private Long last_updated_epoch;
+
+    @SerializedName("last_updated")
+    private String lastUpdated;
+
     @SerializedName("temp_c")
-    private Double tempF;
+    private Double tempC;
+
     @SerializedName("temp_f")
-    private Integer isDay;
+    private Double tempF;
+
     @SerializedName("is_day")
+    private Integer isDay;
+
     private Condition condition;
-    private double windMph;
+
     @SerializedName("wind_mph")
-    private Double windKph;
+    private Double windMph;
+
     @SerializedName("wind_kph")
-    private Double windDegree;
+    private Double windKph;
+
     @SerializedName("wind_degree")
-    private Double windDir;
+    private Integer windDegree;
+
     @SerializedName("wind_dir")
-    private Double pressureMb;
+    private String windDir;
+
     @SerializedName("pressure_mb")
-    private Double pressureIn;
+    private Double pressureMb;
+
     @SerializedName("pressure_in")
-    private Double precipMm;
+    private Double pressureIn;
+
     @SerializedName("precip_mm")
-    private Double precipIn;
+    private Double precipMm;
+
     @SerializedName("precip_in")
-    private Double humidity;
-    private Double cloud;
-    private Double feelslikeC; // Replace this shit
+    private Double precipIn;
+
+    private Integer humidity;
+    private Integer cloud;
+
     @SerializedName("feelslike_c")
-    private Double feelslikeF;
+    private Double feelslikeC;
+
     @SerializedName("feelslike_f")
-    private Double visKm;
+    private Double feelslikeF;
+
     @SerializedName("vis_km")
-    private Double visMiles;
+    private Double visKm;
+
     @SerializedName("vis_miles")
+    private Double visMiles;
+
     private Double uv;
-    private Double gustSpeedMph;
-    @SerializedName("gustMph")
+
+    @SerializedName("gust_mph")
+    private Double gustMph;
+
+    @SerializedName("gust_kph")
     private Double gustKph;
-    @SerializedName("gustKph")
+
+    @SerializedName("air_quality")
     private AirQuality airQuality;
+
+    public Long getLast_updated_epoch() {
+        return last_updated_epoch;
+    }
+
+    public void setLast_updated_epoch(Long last_updated_epoch) {
+        this.last_updated_epoch = last_updated_epoch;
+    }
+
+    public String getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(String lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public Double getTempC() {
+        return tempC;
+    }
+
+    public void setTempC(Double tempC) {
+        this.tempC = tempC;
+    }
+
+    public Double getTempF() {
+        return tempF;
+    }
+
+    public void setTempF(Double tempF) {
+        this.tempF = tempF;
+    }
+
+    public Integer getIsDay() {
+        return isDay;
+    }
+
+    public void setIsDay(Integer isDay) {
+        this.isDay = isDay;
+    }
+
+    public Condition getCondition() {
+        return condition;
+    }
+
+    public void setCondition(Condition condition) {
+        this.condition = condition;
+    }
+
+    public Double getWindMph() {
+        return windMph;
+    }
+
+    public void setWindMph(Double windMph) {
+        this.windMph = windMph;
+    }
+
+    public Double getWindKph() {
+        return windKph;
+    }
+
+    public void setWindKph(Double windKph) {
+        this.windKph = windKph;
+    }
+
+    public Integer getWindDegree() {
+        return windDegree;
+    }
+
+    public void setWindDegree(Integer windDegree) {
+        this.windDegree = windDegree;
+    }
+
+    public String getWindDir() {
+        return windDir;
+    }
+
+    public void setWindDir(String windDir) {
+        this.windDir = windDir;
+    }
+
+    public Double getPressureMb() {
+        return pressureMb;
+    }
+
+    public void setPressureMb(Double pressureMb) {
+        this.pressureMb = pressureMb;
+    }
+
+    public Double getPressureIn() {
+        return pressureIn;
+    }
+
+    public void setPressureIn(Double pressureIn) {
+        this.pressureIn = pressureIn;
+    }
+
+    public Double getPrecipMm() {
+        return precipMm;
+    }
+
+    public void setPrecipMm(Double precipMm) {
+        this.precipMm = precipMm;
+    }
+
+    public Double getPrecipIn() {
+        return precipIn;
+    }
+
+    public void setPrecipIn(Double precipIn) {
+        this.precipIn = precipIn;
+    }
+
+    public Integer getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(Integer humidity) {
+        this.humidity = humidity;
+    }
+
+    public Integer getCloud() {
+        return cloud;
+    }
+
+    public void setCloud(Integer cloud) {
+        this.cloud = cloud;
+    }
+
+    public Double getFeelslikeC() {
+        return feelslikeC;
+    }
+
+    public void setFeelslikeC(Double feelslikeC) {
+        this.feelslikeC = feelslikeC;
+    }
+
+    public Double getFeelslikeF() {
+        return feelslikeF;
+    }
+
+    public void setFeelslikeF(Double feelslikeF) {
+        this.feelslikeF = feelslikeF;
+    }
+
+    public Double getVisKm() {
+        return visKm;
+    }
+
+    public void setVisKm(Double visKm) {
+        this.visKm = visKm;
+    }
+
+    public Double getVisMiles() {
+        return visMiles;
+    }
+
+    public void setVisMiles(Double visMiles) {
+        this.visMiles = visMiles;
+    }
+
+    public Double getUv() {
+        return uv;
+    }
+
+    public void setUv(Double uv) {
+        this.uv = uv;
+    }
+
+    public Double getGustMph() {
+        return gustMph;
+    }
+
+    public void setGustMph(Double gustMph) {
+        this.gustMph = gustMph;
+    }
+
+    public Double getGustKph() {
+        return gustKph;
+    }
+
+    public void setGustKph(Double gustKph) {
+        this.gustKph = gustKph;
+    }
+
+    public AirQuality getAirQuality() {
+        return airQuality;
+    }
+
+    public void setAirQuality(AirQuality airQuality) {
+        this.airQuality = airQuality;
+    }
 }
